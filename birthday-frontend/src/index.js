1266ae2fcce9cc225e19f7afececa56d8fe333ab
mynameisvictor <mynameisvictor@126.com>
2018-04-25 15:43:55 -0400

    V

--- birthday-frontend/src/index.js
+++ birthday-frontend/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const AuthorForm = (props) => {
+    return (
+		<div className="category-form">
+    );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
