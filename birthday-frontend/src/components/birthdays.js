2c1866c5b00cb6b90bbd2ad9d3fbd3cdb604d3ca
Victor SUn <sunvi@mail.gvsu.edu>
2018-04-26 14:13:40 -0400

    V

--- birthday-frontend/src/components/birthdays.js
+++ birthday-frontend/src/components/birthdays.js
@@ -23,7 +23,7 @@ const Birthdays = (props) => {
 }
 
 class Birthdays extends React.Component {
-	
+
 	constructor(props){
 		super(props);
 		const id = props.match.paramd.id;
@@ -38,8 +38,77 @@ class Birthdays extends React.Component {
 	}
 
 	loadBirthdays(){
-	
-	}
+	    axios.get(`${API_BASE}/categories/${this.state.category_id}/birthdays`).then(res => {
+            this.setState({
+                birthdays: res.data
+            });
+            console.log(`Data loaded! = ${this.state.birthdays}`)
+        })
+        .catch(err => console.log(err));
+
+        axios.get(`${API_BASE}/categories/${this.state.category_id}`).then(res => {
+            this.setState({ category: res.data});
+            console.log(`Data loaded! = ${this.state.birthdays}`)
+        })
+        .catch(err => console.log(err));
+    }
+
+    deleteBirthday(id){
+        let filteredArray = this.state.birthdays.filter(item => item.id !== id)
+        this.setState({birthdays: filteredArray});
+        axios.delete(`${API_BASE}/categories/${this.state.category_id}/birthdays/${id}`).then(res => {
+            console.log(`Record Deleted`);
+        })
+        .catch(err => console.log(err));
+    }
+
+    componentDidMount(){
+        console.log('Posts mounted!')
+        this.loadPosts();
+    }
+
+    render(){
+        const postItems = this.state.birthdays.map((birthday) => {
+            return (
+                <BirthdayItem
+                    name = {birthday.name}
+                    date = {birthday.date}
+                    category_id = {birthday.category_id}
+                    id = {birthday.id}
+                    key = {birthday.id}
+                    onDelete = {this.deleteBirthday}
+                />
+            )
+        });
+
+        const headerString = (this.state.birthdays.count === 0) ? "Loading..." : `Posts by ${this.state.category.category}`
+        return (
+            <div className = "birthdays">
+                <h1> {headerString} </h1>
+                <div className = "category-list">
+                    <table className = "table table-hover">
+                        <thead>
+                            <tr>
+                                <th className = "col-md-3">Name</ht>
+                                <th className = "col-md-3">Date</ht>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {postItems}
+                        </tbody>
+                    </table>
+                    <Link to = {`/categories/${this.state.category_id}/posts/create`}>
+                        <button className = "btn btn-sucess btn-sm">
+                            <i className = "glyphicon glyphicon-plus"></i> Create
+                        </button>
+                    </Link>
+                    <button className = "btn btn-danger btn-sm" onClick={() => this.props.history.goBack()}>
+                        <i className = "glyphicon glyphicon-menu-left"></i> Back
+                    </button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Birthdays;
